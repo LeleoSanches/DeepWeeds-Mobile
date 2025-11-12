a5932e38f18e6cf106c89796536afb2a2988602b
Leonardo Goshi <leosanches.ia@gmail.com>
2025-11-12 17:12:55 -0300

    Create functions get_backbone - set_generators - set_tl - set_fn - fitting model - fitting tf

--- pipeline.py
+++ pipeline.py
@@ -42,6 +42,7 @@ RESULTS_DIR = "/home/leo/Documentos/DeepWeeds-master/results/"
 MODELS_DIR = "/home/leo/Documentos/DeepWeeds-master/models/"
 
 # Global - Parâmetros
+MODEL_NAME = "MobilenetVg2"
 BATCH_SIZE = 32
 IMG_SIZE = (224, 224)
 AUTOTUNE = tf.data.AUTOTUNE
@@ -50,23 +51,38 @@ classes = [0,1,2,3,4,5,6,7,8]
 mixed_precision.set_global_policy("mixed_float16")
 
 
-data = pd.read_csv(LABEL_DIR + "labels.csv")
-data['Label'] = data["Label"].astype(str)
-data['Filename'] = data['Filename'].apply(lambda x: os.path.join(IMG_DIR, x))
 
+def load_split_data(label_dir: str, test_size: float):
+    #Load Labels df
+    try:
+        data = pd.read_csv(LABEL_DIR + "labels.csv")
+        data['Label'] = data["Label"].astype(str)
+        data['Filename'] = data['Filename'].apply(lambda x: os.path.join(IMG_DIR, x))
+    
+    except FileNotFoundError as e:
+        print(f"[ERRO] labels.csv não encontrado: {csv_path} "
+              f"(verifique se 'label_dir' está correto).")
+        raise
+    
+    #Train test split
+    df_train, df_val = train_test_split(
+        data, test_size=test_size, stratify=data['Label'], random_state=75
+    )
 
-# 1) Split estratificado - Balanceamento das Classes
-df_train, df_val = train_test_split(
-    data, test_size=0.25, stratify=data['Label'], random_state=75
-)
+    df_train = df_train.reset_index(drop=True)
+    df_val = df_val.reset_index(drop=True)
 
-# 2) Classes consistentes (ordenadas)
-classes = sorted(data['Label'].unique().tolist())
+    # 2) Classes consistentes (ordenadas)
+    classes = sorted(data['Label'].unique().tolist())
+    
+    #Debbug
+    print("[DEBUG] Filename resolvido:", data["Filename"].iloc[0])
+    print(f"[DEBUG] Tamanho total: {len(data)} | Treino: {len(df_train)} | Validação: {len(df_val)} "
+          f"({len(df_val)/len(data)*100:.1f}% val)")
 
-# 3) Preprocess do backbone (NÃO use rescale junto)
-#preprocess_fn = mobilenet_v2.preprocess_input
+    
+    return df_train, df_val, classes
 
-#preprocess_fn = mobilenet_v3.preprocess_input
 
 def get_backbone(name: str, img_size):
     h, w = img_size
@@ -83,164 +99,176 @@ def get_backbone(name: str, img_size):
         base_model = mobilenet_v3.MobileNetV3Small(input_shape=input_shape, include_top=False, weights="imagenet")
     else:
         raise ValueError("--model deve ser: mobilenetv2, mobilenetv3large, mobilenetv3small")
-    base_model =  False
     return preprocess_fn, base_model
 
-preprocess_fn, base_model = get_backbone(name="mobilenetv2", img_size=IMG_SIZE)
 
 # 4) Dois datagens: augment só no treino
+def set_generators(preprocess_fn, df_train, df_val, debbug: bool):
+    
+    #Augmentation
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_fn,
+        fill_mode = 'constant',
+        shear_range = 0.2,
+        rotation_range=360,
+        channel_shift_range=25,
+        width_shift_range=0.05,
+        height_shift_range=0.05,
+        zoom_range=(0.1,1),
+        horizontal_flip=True,
+        brightness_range=(0.75, 1.25)
+    )
+
+    val_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_fn
+    )
+
+    # 5) Generators SEM validation_split/subset
+    train_generator = train_datagen.flow_from_dataframe(
+        dataframe=df_train,
+        x_col="Filename",
+        y_col="Label",
+        target_size=IMG_SIZE,
+        batch_size=BATCH_SIZE,
+        class_mode="categorical",
+        classes=classes,
+        shuffle=True,
+        seed=122
+    )
+
+    val_generator = val_datagen.flow_from_dataframe(
+        dataframe=df_val,
+        x_col="Filename",
+        y_col="Label",
+        target_size=IMG_SIZE,
+        batch_size=BATCH_SIZE,
+        class_mode="categorical",
+        classes=classes,
+        shuffle=False
+    )
+
+    if debbug:
+        print(train_generator.class_indices)
+        print(val_generator.class_indices)
+        
+        assert train_generator.class_indices == val_generator.class_indices
+        assert set(train_generator.filenames).isdisjoint(set(val_generator.filenames))
+
+        print("[DEBBUG] Distribuição treino:", Counter(train_generator.classes))
+        print("[DEBBUG] Distribuição val   :", Counter(val_generator.classes))
+
+    return train_generator, val_generator
 
 
-train_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_fn,
-    fill_mode = 'constant',
-    shear_range = 0.2,
-    rotation_range=360,
-    channel_shift_range=25,
-    width_shift_range=0.05,
-    height_shift_range=0.05,
-    zoom_range=(0.1,1),
-    horizontal_flip=True,
-    brightness_range=(0.75, 1.25)
-)
+def set_transferlearning(base_model):
+
+    base_model.trainable = False
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(0.2)(x)
+    x = Dense(256, activation='relu')(x)
+    outputs = Dense(len(classes), activation='sigmoid', dtype="float32" )(x)
+    model = Model(inputs=base_model.input, outputs=outputs)
+
+    #Compile Model
+    model.compile(
+        optimizer=Adam(learning_rate=1e-3),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+
+    return model
+
+
+def fit_model(model, train_generator, val_generator, name:str, class_weight:bool, epochs:int):
+
+    cbs = [
+        callbacks.ModelCheckpoint("best_head.keras", monitor="val_accuracy",
+                                save_best_only=True, mode="max"),
+        callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5,
+                                    patience=16, min_lr=1e-6),
+        callbacks.EarlyStopping(monitor="val_accuracy", patience=32,
+                                restore_best_weights=True, mode="max"),
+        callbacks.CSVLogger(f"treino_log{name}.csv", append=False),
+        callbacks.TensorBoard(log_dir="tb_logs", histogram_freq=1)
+    ]
+
+    if class_weight:
+        cw = compute_class_weight(
+            class_weight='balanced',
+            classes=np.arange(len(classes)),
+            y=train_generator.classes
+        )
+        class_weight = {i: float(w) for i, w in enumerate(cw)}
 
 
 
-val_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_fn
-)
+        history = model.fit(
+            train_generator,
+            validation_data=val_generator,
+            epochs=epochs,
+            callbacks=cbs,
+            class_weight=class_weight,
+            verbose=1
+        )
+    
+    else:
+        history = model.fit(
+            train_generator,
+            validation_data=val_generator,
+            epochs=epochs,
+            callbacks=cbs,
+            verbose=1
+        )
 
-# 5) Generators SEM validation_split/subset
-train_generator = train_datagen.flow_from_dataframe(
-    dataframe=df_train,
-    x_col="Filename",
-    y_col="Label",
-    target_size=IMG_SIZE,
-    batch_size=BATCH_SIZE,
-    class_mode="categorical",
-    classes=classes,
-    shuffle=True,
-    seed=122
-)
-
-val_generator = val_datagen.flow_from_dataframe(
-    dataframe=df_val,
-    x_col="Filename",
-    y_col="Label",
-    target_size=IMG_SIZE,
-    batch_size=BATCH_SIZE,
-    class_mode="categorical",
-    classes=classes,
-    shuffle=False
-)
-
-# 6) Valida os índices e classes
-print(train_generator.class_indices)
-print(val_generator.class_indices)
-assert train_generator.class_indices == val_generator.class_indices
-assert set(train_generator.filenames).isdisjoint(set(val_generator.filenames))
-
-# 7) class_weight p/ desbalanceamento
-cw = compute_class_weight(
-    class_weight='balanced',
-    classes=np.arange(len(classes)),
-    y=train_generator.classes
-)
-class_weight = {i: float(w) for i, w in enumerate(cw)}
-print("Distribuição treino:", Counter(train_generator.classes))
-print("Distribuição val   :", Counter(val_generator.classes))
-
-##MODEL
-### MOBILENETV2
-base_model = MobileNetV2(input_shape=IMG_SIZE + (3,), include_top=False, weights='imagenet')
-#base_model.trainable = False  # Congela a base inicialmente
-
-
-"""
-base_model = MobileNetV3Large(
-    input_shape=(224, 224, 3),
-    include_top=False,
-    weights='imagenet'
-)
-"""
-
-base_model.trainable = False
-##Transfer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dropout(0.2)(x)
-x = Dense(256, activation='relu')(x)
-outputs = Dense(len(classes), activation='sigmoid', dtype="float32" )(x)
-
-model = Model(inputs=base_model.input, outputs=outputs)
-
-
-
-# 8) Compile + callbacks
-model.compile(
-    optimizer=Adam(learning_rate=1e-3),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
-
-
-
-cbs = [
-    callbacks.ModelCheckpoint("best_head.keras", monitor="val_accuracy",
-                              save_best_only=True, mode="max"),
-    callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5,
-                                patience=16, min_lr=1e-6),
-    callbacks.EarlyStopping(monitor="val_accuracy", patience=32,
-                            restore_best_weights=True, mode="max"),
-    callbacks.CSVLogger("treino_log.csv", append=False),
-    callbacks.TensorBoard(log_dir="tb_logs", histogram_freq=1)
-]
-
-
-history = model.fit(
-    train_generator,
-    validation_data=val_generator,
-    epochs=300,
-    callbacks=cbs,
-#    class_weight=class_weight,   # <— Add pesos nas classes (não utilizar)
-    verbose=1
-)
-
+    return history
 
 
 #Fine-Tunning
-model.load_weights("best_head.keras")
+def set_finetunning(model):
+    model.load_weights("best_head.keras")
 
-unfreeze_from = int(len(base_model.layers) * 0.6)  # ajuste 0.6–0.75 conforme VRAM/estabilidade
-base_model.trainable = True
-for i, L in enumerate(base_model.layers):
-    if i < unfreeze_from:
-        L.trainable = False
-    elif isinstance(L, layers.BatchNormalization):
-        L.trainable = False
-    else:
-        L.trainable = True
+    unfreeze_from = int(len(base_model.layers) * 0.6)  # ajuste 0.6–0.75 conforme VRAM/estabilidade
+    base_model.trainable = True
+    for i, L in enumerate(base_model.layers):
+        if i < unfreeze_from:
+            L.trainable = False
+        elif isinstance(L, layers.BatchNormalization):
+            L.trainable = False
+        else:
+            L.trainable = True
 
-# LR bem baixo + (opcional) weight decay
-opt = optimizers.AdamW(learning_rate=1e-5, weight_decay=1e-5)
+    # LR bem baixo + (opcional) weight decay
+    opt = optimizers.AdamW(learning_rate=1e-5, weight_decay=1e-5)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
+
+    return model
+
+def fit_finetunning(model,train_generator, val_generator, name:str):
+    cbs_ft = [
+        callbacks.ModelCheckpoint(f"best_{name}_finetune.keras", monitor="val_accuracy", mode="max", save_best_only=True),
+        callbacks.EarlyStopping(monitor="val_accuracy", mode="max", patience=20, restore_best_weights=True),
+        callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=4, min_lr=1e-6),
+    ]
+
+    history_ft = model.fit(
+        train_generator,
+        validation_data=val_generator,
+        epochs=100,
+        callbacks=cbs_ft,
+        verbose=1
+    )
+    return history_ft
 
 
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
-
-cbs_ft = [
-    callbacks.ModelCheckpoint("best_v3_finetune.keras", monitor="val_accuracy", mode="max", save_best_only=True),
-    callbacks.EarlyStopping(monitor="val_accuracy", mode="max", patience=20, restore_best_weights=True),
-    callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=4, min_lr=1e-6),
-]
-
-history_ft = model.fit(
-    train_generator,
-    validation_data=val_generator,
-    epochs=100,
-    callbacks=cbs_ft,
-    verbose=1
-)
-
+df_train, df_val, classes = load_split_data(label_dir = LABEL_DIR, test_size=0.2)
+preprocess_fn, base_model = get_backbone(name=MODEL_NAME, img_size=IMG_SIZE)
+train_generator, val_generator = set_generators(preprocess_fn, df_train, df_val, debbug=True)
+model = set_transferlearning(base_model)
+history = fit_model(model, train_generator, val_generator, name = MODEL_NAME,class_weight=False,epochs=300 )
+model = set_finetunning(model)
+history_ft = fit_finetunning(model, train_generator, val_generator)
 
 
 ######
@@ -251,29 +279,28 @@ print('iniciando Plotting Results')
 plots = utils()
 
 
-plots.plot_training_curves(history, out_png="curvas_mobilenetv3.png", title="MobileNetV3 - DeepWeeds")
-plots.save_history_csv(history, out_csv="historico_mobilenetv3.csv")
+plots.plot_training_curves(history, out_png=f"curvas_{MODEL_NAME}.png", title="MobileNetV3 - DeepWeeds")
+plots.save_history_csv(history, out_csv=f"historico_{MODEL_NAME}.csv")
 
 plots.plot_confusion_and_report(
     model, val_generator, train_generator.class_indices,
-    cm_png="cm_mobilenetv3.png",
-    report_txt="report_mobilenetv3.txt",
+    cm_png=f"cm_{MODEL_NAME}.png",
+    report_txt=f"report_{MODEL_NAME}.txt",
     normalize=True
 )
 
 
-plots.plot_training_curves(history_ft, out_png="curvas_mobilenetv3_finetunning.png", title="MobileNetV3 Tuned- DeepWeeds")
-plots.save_history_csv(history_ft, out_csv="historico_mobilenetv3_finetunning.csv")
+plots.plot_training_curves(history_ft, out_png=f"curvas_{MODEL_NAME}_finetunning.png", title=f"{MODEL_NAME} Tuned- DeepWeeds")
+plots.save_history_csv(history_ft, out_csv=f"historico_{MODEL_NAME}_finetunning.csv")
 
-#Joining Traning + Fine Tunning Results
 
 joined_dict, df_hist = plots.concat_histories(history, history_ft)
-df_hist.to_csv("historico_joined.csv", index=False)
+df_hist.to_csv(f"historico_{MODEL_NAME}_joined.csv", index=False)
 
 # split_epoch = número de épocas da fase head (ex.: len(history_head.history["accuracy"]))
 split_epoch = len(history.history.get("accuracy", []))
-plots.plot_history_joined(joined_dict, out_png="curvas_head_plus_ft.png",
-                    title="MobileNetV3 - DeepWeeds (Head + FT)",
+plots.plot_history_joined(joined_dict, out_png=f"{MODEL_NAME}_head_plus_ft.png",
+                    title=f"{MODEL_NAME} - DeepWeeds (Head + FT)",
                     split_epoch=split_epoch)
 
 
